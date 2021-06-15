96dc1afdd51e50d5f44b7fd805469161820c7483
aaronconway7 <aaronconway796@gmail.com>
2021-06-15 13:17:17 +0100

    error page

---
+++ layouts/error.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+    div.bluebg.container
+        h1(v-if="error.statusCode === 404") 404: Page not found
+        h1(v-else) An error occurred
+</template>
+
+<script>
+export default {
+    props: ['error'],
+    layout: 'error', // you can set a custom layout for the error page
+}
+</script>
+
+<style lang="sass" scoped>
+.container
+    height: 300px
+    display: grid
+    place-items: center
+</style>
