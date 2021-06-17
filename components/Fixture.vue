6a33e454968befdaaaace2d1fc6c0dc5e474e0f9
aaronconway7 <aaronconway796@gmail.com>
2021-06-17 22:15:38 +0100

    sending offs and not showing missed pens

--- components/Fixture.vue
+++ components/Fixture.vue
@@ -2,11 +2,17 @@
     tr
         td.cyan.team-name {{ fixture.teams.home.name }}
             ul.white
-                li(v-for="goal in homeGoals") {{ goal.player.name }} {{ goal.detail === `Penalty` ? `pen` : `` }} {{ goal.time.elapsed }}
+                li(v-for="goal in homeGoals") {{ goal.player.name }} {{ detailLookup[goal.detail] }} {{ goal.time.elapsed }}
+            ul.red.sent-off(v-if="homeSendingOffs.length > 0")
+                li Sent off:
+                li(v-for="sendingOff in homeSendingOffs") {{ sendingOff.player.name }} {{ sendingOff.time.elapsed }}
         td {{ Number.isInteger(fixture.goals.home) && Number.isInteger(fixture.goals.away) ? `${fixture.goals.home}-${fixture.goals.away}` : `v` }}
         td.cyan.team-name {{ fixture.teams.away.name }}
             ul.white
-                li(v-for="goal in awayGoals") {{ goal.player.name }} {{ goal.detail === `Penalty` ? `pen` : `` }} {{ goal.time.elapsed }}
+                li(v-for="goal in awayGoals") {{ goal.player.name }} {{ detailLookup[goal.detail] }} {{ goal.time.elapsed }}
+            ul.red.sent-off(v-if="awaySendingOffs.length > 0")
+                li Sent off:
+                li(v-for="sendingOff in awaySendingOffs") {{ sendingOff.player.name }} {{ sendingOff.time.elapsed }}
         td.status {{ fixture.fixture.status.short === `NS` ? getKickOffTime(fixture.fixture.date) : fixture.fixture.status.short }}
 </template>
 
@@ -20,23 +26,49 @@ export default {
     },
     data() {
         return {
-            goals: [],
+            events: [],
+            detailLookup: {
+                Penalty: `pen`,
+                'Own Goal': `og`,
+            },
+            validSendingOff: [`Second Yellow card`, `Red card`],
         }
     },
     computed: {
+        homeEvents() {
+            return this.events.filter(
+                (e) => e.team.id === this.fixture.teams.home.id
+            )
+        },
+        awayEvents() {
+            return this.events.filter(
+                (e) => e.team.id === this.fixture.teams.away.id
+            )
+        },
         homeGoals() {
-            return this.goals.filter(
-                (g) => g.team.id === this.fixture.teams.home.id
+            return this.homeEvents.filter(
+                (e) => e.type === `Goal` && e.detail !== `Missed Penalty`
             )
         },
         awayGoals() {
-            return this.goals.filter(
-                (g) => g.team.id === this.fixture.teams.away.id
+            return this.awayEvents.filter(
+                (e) => e.type === `Goal` && e.detail !== `Missed Penalty`
+            )
+        },
+        homeSendingOffs() {
+            return this.homeEvents.filter(
+                (e) =>
+                    e.type === `Card` && this.validSendingOff.includes(e.detail)
+            )
+        },
+        awaySendingOffs() {
+            return this.awayEvents.filter(
+                (e) =>
+                    e.type === `Card` && this.validSendingOff.includes(e.detail)
             )
         },
     },
     async fetch() {
-        // Get Goals
         const data = await this.$http.$get(
             `https://v3.football.api-sports.io/fixtures/events?fixture=${this.fixture.fixture.id}`,
             {
@@ -45,7 +77,7 @@ export default {
                 },
             }
         )
-        this.goals = await data.response.filter((e) => e.type === `Goal`)
+        this.events = data.response
     },
     fetchOnServer: false,
     methods: {
@@ -79,4 +111,7 @@ tr
 ul
     list-style-type: none
     text-transform: initial
+
+    &.sent-off
+        margin-top: 1em
 </style>
